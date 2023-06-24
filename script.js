bc1eff28333ab72411310d7e81d15df6a62cee13
Nitu Dubey <108494260+Nitu170287@users.noreply.github.com>
2023-06-23 23:09:04 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let saveInput = document.querySelector('input[type="submit"]')
+saveInput.addEventListener("click",()=>{
+	let fontSize = document.querySelector('#fontsize').value
+	let fontColor = document.querySelector('input[type="color"]').value
+	// console.log(fontSize,fontColor)
+	document.cookie = "fontColor="+fontColor+"; expires= Fri, 25 June 2023 12:00:00 UTC; path=/";
+	document.cookie = "fontSize="+fontSize+"; expires= Fri, 25 June 2023 12:00:00 UTC; path=/";
+})
